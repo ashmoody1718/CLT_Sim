1765774bd75056298d4cdf57e8f31130828e1e32
ashmoody1718 <ashmoody1718@gmail.com>
2026-07-17 12:33:58 -0700

    Base streamlit app

--- base_app.py
+++ base_app.py
@@ -32,7 +32,6 @@ fig = px.bar(cat_coin_df,
              labels={"0":"Tails", "1":"Heads"})
 fig.update_traces(marker_color='firebrick')
 
-x = 1
 st.plotly_chart(fig, use_container_width=True)
 
 #
